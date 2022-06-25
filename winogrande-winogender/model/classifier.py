e676a158d59798dcbcb9eb711d3c198a98323d79
Alex Mallen <atmallen8@gmail.com>
2022-06-25 00:26:03 -0400

    pivoting research directions

--- winogrande-winogender/model/classifier.py
+++ winogrande-winogender/model/classifier.py
@@ -1,14 +1,15 @@
 from wrangl.learn import SupervisedModel, metrics as M
 import torch
-from transformers import AutoModelForSequenceClassification as AutoModel, AutoTokenizer
-
+from torch.nn import functional as F
+from transformers import GPT2LMHeadModel as AutoModel, AutoTokenizer
 
 class Model(SupervisedModel):
 
     def __init__(self, cfg):
         super().__init__(cfg)
-        self.lm = AutoModel.from_pretrained(cfg.lm, num_labels=2)
+        self.lm = AutoModel.from_pretrained(cfg.lm)
         self.tokenizer = AutoTokenizer.from_pretrained(cfg.lm)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
         self.acc = M.Accuracy()
 
     def featurize(self, batch: list):
@@ -18,8 +19,10 @@ class Model(SupervisedModel):
 
         Alternatively you may want to set `collate_fn: "ignore"` in your config and use `featurize` to convert raw examples into features.
         """
-        sentence = self.tokenizer(
-            [ex['sentence'] + ' choice ' + ex['option1'] + ' choice ' + ex['option2'] for ex in batch],
+        for ex in batch:
+
+         correct_sentence = self.tokenizer(
+            [ex['sentence'].replace('_', ex["option1" if ex["answer"] == '1' else "option2"]) for ex in batch],
             return_tensors='pt',
             add_special_tokens=True,
             truncation=True,
@@ -27,12 +30,19 @@ class Model(SupervisedModel):
             return_attention_mask=True,
             max_length=self.hparams.max_context_length,
         ).to(self.device)
-        mapping = {'1': 0, '2': 1}
-        labels = torch.tensor([mapping[ex['answer']] for ex in batch], device=self.device)
-        return dict(sentence=sentence, labels=labels)
+        incorrect_sentence = self.tokenizer(
+            [ex['sentence'].replace('_', ex["option1" if ex["answer"] == '2' else "option2"]) for ex in batch],
+            return_tensors='pt',
+            add_special_tokens=True,
+            truncation=True,
+            padding=True,
+            return_attention_mask=True,
+            max_length=self.hparams.max_context_length,
+        ).to(self.device)
+        return dict(correct_sentence=correct_sentence, incorrect_sentence=incorrect_sentence)
 
     def compute_loss(self, out, feat, batch):
-        return out.loss
+        return out["loss"]
 
     def compute_metrics(self, pred: list, gold: list, batch: list = None) -> dict:
         # this returns a dictionary {'val_acc': x}
@@ -44,21 +54,25 @@ class Model(SupervisedModel):
     def extract_pred(self, out, feat, batch):
         pred = []
         for p, ex in zip(out, batch):
-            if p == 0:
-                pred.append(ex['option1'])
-            elif p == 1:
-                pred.append(ex['option2'])
-            else:
-                raise NotImplementedError('Unknown prediction {} for example {}'.format(p, ex))
+            pred.append(ex['sentence'].replace('_', ex["option1"] if p == 0 else "option2"))
         return pred
 
     @classmethod
     def extract_gold(cls, feat, batch):
-        return [ex['option{}'.format(ex['answer'])] for ex in batch]
+         return [ex['sentence'].replace('_', ex["option1" if ex["answer"] == '1' else "option2"]) for ex in batch]
 
     def forward(self, feat, batch):
-        out = self.lm(feat['sentence']['input_ids'], attention_mask=feat['sentence']['attention_mask'], labels=feat['labels'])
-        return out
+        correct_out = self.lm(feat['correct_sentence']['input_ids'], attention_mask=feat['correct_sentence']['attention_mask'], labels=feat['correct_sentence']['input_ids'])
+        incorrect_out = self.lm(feat['incorrect_sentence']['input_ids'], attention_mask=feat['incorrect_sentence']['attention_mask'], labels=feat['incorrect_sentence']['input_ids'])
+        loss = correct_out["loss"] - incorrect_out["loss"]
+        return dict(loss=loss, logits=None)
 
     def infer(self, feat, batch):
-        return self.lm(feat['sentence']['input_ids'], attention_mask=feat['sentence']['attention_mask']).logits.max(1)[1].tolist()
+        ps = []
+        for i in range(len(batch)):
+            correct_out = self.lm(feat['correct_sentence']['input_ids'][i], attention_mask=feat['correct_sentence']['attention_mask'][i], labels=feat['correct_sentence']['input_ids'][i])
+            incorrect_out = self.lm(feat['incorrect_sentence']['input_ids'][i], attention_mask=feat['incorrect_sentence']['attention_mask'][i], labels=feat['incorrect_sentence']['input_ids'][i])
+            loss = correct_out["loss"] - incorrect_out["loss"]
+            ps.append(int(loss > 0))  # 0 means first (correct) sentence
+        return ps
+
